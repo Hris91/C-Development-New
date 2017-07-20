24f77ea00021fa291070da9e96628ef91e9aa5d7
Hristiyan <dogman19991@gmail.com>
2017-07-20 18:32:18 +0300

    Lab problem 2.

---
+++ C# OOP Advanced/Labs/02.Cars/Seat.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+public class Seat : ICar
+{
+    public string Model { get; private set; }
+    public string Color { get; private set; }
+
+    public Seat(string model, string color)
+    {
+        this.Model = model;
+        this.Color = color;
+    }
+
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+    public override string ToString()
+    {
+        var builder = new StringBuilder();
+        builder.AppendLine($"{this.Color} Seat {this.Model}");
+        builder.AppendLine(Start());
+        builder.Append(Stop());
+
+        return builder.ToString();
+    }
+}
---
+++ C# OOP Advanced/Labs/02.Cars/Tesla.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+public class Tesla : ICar, IElectricCar
+{
+    public string Model { get; private set; }
+    public string Color { get; private set; }
+    public int Battery { get; private set; }
+
+    public Tesla(string model, string color, int battery)
+    {
+        this.Model = model;
+        this.Color = color;
+        this.Battery = battery;
+    }
+
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+    public override string ToString()
+    {
+        var builder = new StringBuilder();
+        builder.AppendLine($"{this.Color} Tesla {this.Model} with {this.Battery} Batteries");
+        builder.AppendLine(Start());
+        builder.Append(Stop());
+
+        return builder.ToString();
+    }
+}
