23365660d3d3ef31dca7c29e25e5e6a526917778
Hristiyan <dogman19991@gmail.com>
2017-07-26 14:27:54 +0300

    Some Interfaces exercises.

---
+++ C# OOP Advanced/Exercises/04. Telephony/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Telephony
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var phoneNumbers = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            var websites = Console.ReadLine()
+               .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+               .ToList();
+
+            var smartphone = new Smartphone();
+
+            phoneNumbers
+                .ForEach(p => smartphone.AddNumber(p));
+
+            websites
+                .ForEach(w => smartphone.AddSite(w));
+
+            smartphone.Print();
+        }
+    }
+}
