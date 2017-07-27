595d8785a9471fac8df58c73469923cd64e43f1e
Hristiyan <dogman19991@gmail.com>
2017-07-28 01:19:56 +0300

    some exercise in generics

---
+++ C# OOP Advanced/Exercises/08. Custom List/ICustumList.cs
@@ -0,0 +1,21 @@
+﻿namespace _08.Custom_List
+{
+    public interface ICustumList<T>
+    {
+        void Add(T element);
+
+        T Remove(int index);
+
+        bool Contains(T element);
+
+        void Swap(int index1, int index2);
+
+        int CountGreaterThan(T element);
+
+        T Max();
+
+        T Min();
+
+        void Print();
+    }
+}
---
+++ C# OOP Advanced/Exercises/08. Custom List/Sorter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.Custom_List
+{
+    public static class Sorter
+    {
+        public static CustumList<T> Sort<T>(CustumList<T> custumList)
+            where T : IComparable<T>
+        {
+            var temp = custumList.ListOfElements.OrderBy(x => x);
+            CustumList<T> list = new CustumList<T>(temp);
+            return list;
+        }
+    }
+}
